138df728016e2c59c0116ba8524465c6eadadc1b
Mohamed Traore <133103465+mohtr@users.noreply.github.com>
2023-07-13 16:25:50 +0200

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -9,7 +9,8 @@ def factorial(n):
     for i in range(1, n + 1):
         factorial = factorial * i
     return factorial
-    
+
+
 def sum_factorial():
     for i in range(50):
         final_list.append(factorial(i))
